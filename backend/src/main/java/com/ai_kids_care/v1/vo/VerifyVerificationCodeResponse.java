fd7d4384124a0af530d8a0fd3fa83c6754455a58
Zhang Junfan 장준범 <925209421@qq.com>
2026-03-17 15:42:56 +0900

    backend: VO

---
+++ backend/src/main/java/com/ai_kids_care/v1/vo/VerifyVerificationCodeResponse.java
@@ -0,0 +1,37 @@
+package com.ai_kids_care.v1.vo;
+
+import lombok.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.OffsetDateTime;
+import org.springframework.format.annotation.DateTimeFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import jakarta.annotation.Generated;
+
+/**
+ * VerifyVerificationCodeResponse
+ */
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", comments = "Generator version: 7.20.0")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class VerifyVerificationCodeResponse {
+
+  
+  @NonNull
+  @Schema(name = "verificationToken", description = "Short-lived token proving the challenge was verified", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("verificationToken")
+  private String verificationToken;
+
+  
+  @NonNull
+  @Schema(name = "expiresAt", description = "Token expiration time (UTC recommended)", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("expiresAt")
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+  private OffsetDateTime expiresAt;
+
+}
+
