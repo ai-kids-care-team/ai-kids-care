f51b1522e7fa3a76a640e582140a7e5ba18ecd5d
Zhang Junfan 장준범 <925209421@qq.com>
2026-03-18 19:29:09 +0900

    refactor old DTO

--- backend/src/main/java/com/ai_kids_care/v1/dto/AuthRegisterRequest.java
+++ backend/src/main/java/com/ai_kids_care/v1/dto/AuthRegisterRequest.java
@@ -1,49 +1,51 @@
 package com.ai_kids_care.v1.dto;
 
-import lombok.*;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
+import com.ai_kids_care.v1.entity.User;
+import com.ai_kids_care.v1.type.UserRoleEnum;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
-import jakarta.annotation.Generated;
+import java.io.Serializable;
 
 /**
- * At least one of loginId/email/phone must be provided (enforced by server).
+ * DTO for {@link User}
  */
-@Schema(name = "AuthRegisterRequest", description = "At least one of loginId/email/phone must be provided (enforced by server).")
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", comments = "Generator version: 7.20.0")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class AuthRegisterRequest {
-
-  
-  @Schema(name = "loginId", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-  @JsonProperty("loginId")
-  private String loginId = null;
-
-  
-  @Schema(name = "email", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-  @JsonProperty("email")
-  private String email = null;
-
-  
-  @Schema(name = "phone", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-  @JsonProperty("phone")
-  private String phone = null;
-
-  
-  @ToString.Exclude
-  @NonNull
-  @Schema(name = "password", requiredMode = Schema.RequiredMode.REQUIRED)
-  @JsonProperty("password")
-  private String password;
-
-  
-  @Schema(name = "verification", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-  @JsonProperty("verification")
-  private AuthRegisterRequestVerification verification = null;
-
+public class AuthRegisterRequest implements Serializable {
+    @NotNull(message = "로그인 ID를 입력해주세요.")
+    @NotBlank(message = "로그인 ID를 입력해주세요.")
+    private String loginId;
+    @Email
+    private String email;
+    private String phone;
+    @NotNull(message = "비밀번호를 입력해주세요.")
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    private String password;
+    @NotNull(message = "이름을 입력해주세요.")
+    @NotBlank(message = "이름을 입력해주세요.")
+    private String name;
+    @NotNull(message = "회원유형을 선택해주세요.")
+    @NotBlank(message = "회원유형을 선택해주세요.")
+    private UserRoleEnum userRole;
+    private Long childId;
+    private String rrnFirst6;
+    private String rrnBack7;
+    private String relationship;
+    private String customRelationship;
+    private Boolean primaryGuardian;
+    private String department;
+    private Long kindergartenId;
+    private String staffNo;
+    private String gender;
+    private String emergencyContactName;
+    private String emergencyContactPhone;
+    private String level;
+    private String startDate;
+    private String endDate;
 }
-
