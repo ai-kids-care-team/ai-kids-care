cffc7f0ba0267c8949d10cba84530faa71db75ff
korea4050-debug <korea4050@naver.com>
2026-03-27 18:28:17 +0900

    테스용 user
    테스용 user

--- db/initdb/00_init.sql
+++ db/initdb/00_init.sql
@@ -7,3 +7,27 @@ DECLARE
 BEGIN
     EXECUTE format('ALTER DATABASE %I SET timezone TO %L', db_name, 'Asia/Seoul');
 END $$;
+
+
+
+-- 시스템관리자
+-- PLATFORM_IT_ADMIN,12,hcheetamb
+
+
+--행정청
+-- SUPERADMIN,1,admin
+
+-- 유치원 - 원장님
+-- KINDERGARTEN_ADMIN,101,enozzolii2s
+-- KINDERGARTEN_ADMIN,401,ekemmeb4
+-- KINDERGARTEN_ADMIN,701,rpattonjg
+
+-- 유치원 - 선생님
+-- TEACHER,102,atithecote2t
+
+-- 양육자
+-- GUARDIAN,135,lmount3q
+
+-- 양육자
+-- GUARDIAN,144,cpaulo3z
+
