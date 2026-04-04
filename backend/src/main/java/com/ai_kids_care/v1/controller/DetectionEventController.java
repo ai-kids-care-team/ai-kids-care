7cf3579bca36ddb4e5b75e192ae4fc080711c75b
Zhang Junfan 장준범 <925209421@qq.com>
2026-04-05 01:39:30 +0900

    Detection Event

--- backend/src/main/java/com/ai_kids_care/v1/controller/DetectionEventController.java
+++ backend/src/main/java/com/ai_kids_care/v1/controller/DetectionEventController.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name="DetectionEvent")
@@ -23,7 +22,6 @@ public class DetectionEventController {
 
     private final DetectionEventService service;
 
-    @Transactional(readOnly = true)
     @GetMapping
     public ResponseEntity<Page<DetectionEventVO>> listDetectionEvent(
             @RequestParam(required = false) String keyword,
@@ -32,7 +30,6 @@ public class DetectionEventController {
         return ResponseEntity.ok(service.listDetectionEvents(keyword, pageable));
     }
 
-    @Transactional(readOnly = true)
     @GetMapping("/{id}")
     public ResponseEntity<DetectionEventVO> getDetectionEvent(@PathVariable Long id) {
         return ResponseEntity.ok(service.getDetectionEvent(id));
@@ -43,7 +40,6 @@ public class DetectionEventController {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.createDetectionEvent(createDTO));
     }
 
-    @Transactional
     @PutMapping("/{id}")
     public ResponseEntity<DetectionEventVO> updateDetectionEvent(
             @PathVariable Long id,
--- backend/src/main/java/com/ai_kids_care/v1/repository/DetectionEventRepository.java
+++ backend/src/main/java/com/ai_kids_care/v1/repository/DetectionEventRepository.java
@@ -1,13 +1,7 @@
 package com.ai_kids_care.v1.repository;
 
 import com.ai_kids_care.v1.entity.DetectionEvent;
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DetectionEventRepository extends JpaRepository<DetectionEvent, Long> {
-
-    @Override
-    Page<DetectionEvent> findAll(Pageable pageable);
 }
